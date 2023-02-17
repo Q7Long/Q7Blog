3dde59fd86f030c75c684345824482b23df0b6b0
Q7Long <195228820@qq.com>
2023-02-17 20:30:12 +0800

    feat(q7long):add content

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -79,7 +79,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: "浏览器工作原理", link: "/pages/4c778760be26d8b3/" },
           { text: "NodeJs", link: "/pages/117708e0af7f0bd9/" },
           { text: "HTTP协议", link: "/pages/41f87d890d0a02af/" },
-          { text: "计算机组成原理", link: "/pages/5973a0/" },
+          { text: "计算机408系列", link: "/pages/5973a0/" },
           { text: "重学前端", link: "/pages/b84cfb/" },
           { text: "日常积累", link: "/pages/df305d/" },
         ],
